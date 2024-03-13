62f8f763448c39795e2dd31e38c5d91e7b221078
rkdeofuf <asa47972000@naver.com>
2024-03-13 19:01:34 +0900

    #22 - 도메인 파라미터 순서 수정, 코드 문맥 수정

--- board-project/board/src/test/java/com/example/board/repository/JpaRepositoryTest.java
+++ board-project/board/src/test/java/com/example/board/repository/JpaRepositoryTest.java
@@ -1,45 +1,43 @@
 package com.example.board.repository;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.board.config.JpaConfig;
 import com.example.board.domain.Article;
+import com.example.board.domain.UserAccount;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
+
   private final ArticleRepository articleRepository;
   private final ArticleCommentRepository articleCommentRepository;
-
+  private final UserAccountRepository userAccountRepository;
 
   public JpaRepositoryTest(
       @Autowired ArticleRepository articleRepository,
-      @Autowired ArticleCommentRepository articleCommentRepository) {
+      @Autowired ArticleCommentRepository articleCommentRepository,
+      @Autowired UserAccountRepository userAccountRepository) {
     this.articleRepository = articleRepository;
     this.articleCommentRepository = articleCommentRepository;
+    this.userAccountRepository = userAccountRepository;
   }
 
   @DisplayName("select test")
   @Test
-  void givenTestData_whenSelecting_thenWorksFine () {
+  void givenTestData_whenSelecting_thenWorksFine() {
     // Given
-
 
     // When
     List<Article> articles = articleRepository.findAll();
-
 
     // Then
     assertThat(articles).isNotNull().hasSize(100);
@@ -47,12 +45,15 @@ class JpaRepositoryTest {
 
   @DisplayName("insert test")
   @Test
-  void givenTestData_whenInserting_thenWorksFine () {
+  void givenTestData_whenInserting_thenWorksFine() {
     // Given
     long previousCount = articleRepository.count();
-
+    UserAccount userAccount = userAccountRepository.save(
+        UserAccount.of("uno", "pw", null, null, null));
+    Article article = Article.of(userAccount, "new article", "new content", "#spring");
     // When
-    Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+    Article savedArticle = articleRepository.save(
+        article);
 
     // Then
     assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
@@ -60,7 +61,7 @@ class JpaRepositoryTest {
 
   @DisplayName("update test")
   @Test
-  void givenTestData_whenUpdating_thenWorksFine () {
+  void givenTestData_whenUpdating_thenWorksFine() {
     // Given
     Article article = articleRepository.findById(1L).orElseThrow();
     String updatedHashtag = "#Springboot";
@@ -75,7 +76,7 @@ class JpaRepositoryTest {
 
   @DisplayName("delete test")
   @Test
-  void givenTestData_whenDeleting_thenWorksFine () {
+  void givenTestData_whenDeleting_thenWorksFine() {
     // Given
     Article article = articleRepository.findById(1L).orElseThrow();
     long prevArticleCount = articleRepository.count();
