5be4e28a103b9faa34726fa37e47ec59cc3c759f
rkdeofuf <asa47972000@naver.com>
2024-03-13 22:09:42 +0900

    #22 - sql 데이터 형식 수정
    도메인 일부 수정으로 인한 재수정

--- board-project/board/src/main/java/com/example/board/domain/Article.java
+++ board-project/board/src/main/java/com/example/board/domain/Article.java
@@ -33,18 +33,23 @@ public class Article extends AuditingFields {
   @OrderBy("createdAt DESC")
   @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
   private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @Setter
   @ManyToOne(optional = false)
   private UserAccount userAccount;
+
   @Setter
   @Column(nullable = false)
   private String title; // 제목
+
   @Setter
   @Column(nullable = false, length = 10000)
   private String content; // 본문
+
   @Setter
   private String hashtag; // 해시태그
 
