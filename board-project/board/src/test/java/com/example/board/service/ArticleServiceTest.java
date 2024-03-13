155405071eafbff463b53832cd5038215acbdbd2
rkdeofuf <asa47972000@naver.com>
2024-03-13 22:07:37 +0900

    #22 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- board-project/board/src/test/java/com/example/board/service/ArticleServiceTest.java
+++ board-project/board/src/test/java/com/example/board/service/ArticleServiceTest.java
@@ -38,7 +38,7 @@ class ArticleServiceTest {
   @Mock
   private ArticleRepository articleRepository;
   @Mock
-  private ArticleCommentRepository articleCommentRepositoryRepository;
+  private ArticleCommentRepository articleCommentRepository;
 
   @DisplayName("검색어 없이 게시글을 검색하면, 게시글 페이지를 반환한다.")
   @Test
@@ -62,14 +62,15 @@ class ArticleServiceTest {
     SearchType searchType = SearchType.TITLE;
     String searchKeyword = "title";
     Pageable pageable = Pageable.ofSize(20);
-    given(articleRepository.findByTitle(searchKeyword, pageable)).willReturn(Page.empty());
+    given(articleRepository.findByTitleContaining(searchKeyword, pageable)).willReturn(
+        Page.empty());
 
     // When
     Page<ArticleDto> articles = sut.searchArticles(searchType, searchKeyword, pageable);
 
     // Then
     assertThat(articles).isEmpty();
-    then(articleRepository).should().findByTitle(searchKeyword, pageable);
+    then(articleRepository).should().findByTitleContaining(searchKeyword, pageable);
   }
 
   @DisplayName("게시글을 조회하면, 게시글을 반환한다.")
@@ -102,7 +103,7 @@ class ArticleServiceTest {
 
     // Then
     assertThat(t).isInstanceOf(EntityNotFoundException.class)
-        .hasMessage("게시글이 없습니다 - articleId: " + articleId);
+        .hasMessage("게시글이 없습니다: article id");
     then(articleRepository).should().findById(articleId);
   }
 
