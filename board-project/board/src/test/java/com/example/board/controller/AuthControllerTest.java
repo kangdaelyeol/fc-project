3f4c5a857f87740ee40849aefed83d5989424fde
rkdeofuf <asa47972000@naver.com>
2024-03-18 18:23:40 +0900

    #45 - 원인을 알 수 없는 소스코드 유실 확인하기

--- board-project/board/src/test/java/com/example/board/controller/AuthControllerTest.java
+++ board-project/board/src/test/java/com/example/board/controller/AuthControllerTest.java
@@ -1,24 +1,36 @@
 package com.example.board.controller;
 
-import com.example.board.config.SecurityConfig;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.board.config.TestSecurityConfig;
+import com.example.board.service.ArticleService;
+import com.example.board.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 인증 기능")
-@Import(SecurityConfig.class)
-@WebMvcTest
+@Import(TestSecurityConfig.class)
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
 
   private final MockMvc mvc;
+  @MockBean
+  private ArticleService articleService;
+  @MockBean
+  private PaginationService paginationService;
 
-  public AuthControllerTest(@Autowired MockMvc mvc){this.mvc = mvc;}
+  public AuthControllerTest(@Autowired MockMvc mvc) {
+    this.mvc = mvc;
+  }
 
   @DisplayName("[view] [GET] 로그인 페이지 - 정상 호출")
   @Test
@@ -29,5 +41,7 @@ public class AuthControllerTest {
     mvc.perform(get("/login"))
         .andExpect(status().isOk())
         .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    then(articleService).shouldHaveNoInteractions();
+    then(paginationService).shouldHaveNoInteractions();
   }
 }
